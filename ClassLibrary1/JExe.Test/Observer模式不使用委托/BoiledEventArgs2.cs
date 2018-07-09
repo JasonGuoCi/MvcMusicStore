ac2e789d50c91c95bb73f9ce263b8e06b168096c
JasonGuoCi <geqianci@163.com>
2018-07-09 17:29:03 +0800

    add BoiledEventArgs2

---
+++ ClassLibrary1/JExe.Test/Observer模式不使用委托/BoiledEventArgs2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JExe.Test.Observer模式不使用委托
+{
+    public class BoiledEventArgs2
+    {
+        private int temperature;
+        private string type;
+        private string area;
+
+        public BoiledEventArgs2(string type, string area, int temperature)
+        {
+            this.temperature = temperature;
+            this.type = type;
+            this.area = area;
+        }
+
+        public int Temperature { get { return temperature; } }
+        public string Type { get { return type; } }
+        public string Area { get { return area; } }
+    }
+}
