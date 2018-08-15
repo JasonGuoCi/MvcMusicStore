4ed88c8ac4845adb5ed47ad10d40384baf0ad5ec
JasonGuoCi <geqianci@163.com>
2018-08-15 15:10:10 +0800

    server accecp client

--- ClassLibrary1/ServerConsole/Program.cs
+++ ClassLibrary1/ServerConsole/Program.cs
@@ -18,6 +18,13 @@ namespace ServerConsole
             TcpListener listener = new TcpListener(ip, 8500);
             listener.Start();
             Console.WriteLine("Listener is starting...");
+            //获取一个连接，中断方法
+            TcpClient remoteClient = listener.AcceptTcpClient();
+
+            // 打印连接到的客户端信息
+            Console.WriteLine("Client Connected! Local:{0} <-- Client:{1}",
+            remoteClient.Client.LocalEndPoint, remoteClient.Client.RemoteEndPoint);
+            // 按Q退出
             Console.WriteLine("Type Q key to exist....");
 
             ConsoleKey key;
