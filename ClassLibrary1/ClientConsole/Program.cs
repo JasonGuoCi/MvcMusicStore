b54de34924c1472a5417c3ef85c2ae47c52a3469
JasonGuoCi <geqianci@163.com>
2018-08-15 15:05:21 +0800

    tcp socket listener

---
+++ ClassLibrary1/ClientConsole/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Client is running ...");
+            TcpClient client = new TcpClient();
+            try
+            {
+                // 与服务器建立连接
+                client.Connect(IPAddress.Parse("192.168.1.7"), 8500);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            // 打印连接到的服务端信息
+            Console.WriteLine("Server Connected! Local:{0} -->Server:{1}",
+            client.Client.LocalEndPoint, client.Client.RemoteEndPoint);
+            // 按Q退出
+            ConsoleKey key;
+            do
+            {
+                key = Console.ReadKey(true).Key;
+            } while (key != ConsoleKey.Q);
+        }
+    }
+}
---
+++ ClassLibrary1/ServerConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server is running...");
+            IPAddress ip = new IPAddress(new byte[] { 192, 168, 1, 7 });
+
+            TcpListener listener = new TcpListener(ip, 8500);
+            listener.Start();
+            Console.WriteLine("Listener is starting...");
+            Console.WriteLine("Type Q key to exist....");
+
+            ConsoleKey key;
+            do
+            {
+                key = Console.ReadKey(true).Key;
+            } while (key != ConsoleKey.Q);
+        }
+    }
+}
