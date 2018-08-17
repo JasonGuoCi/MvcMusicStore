4423aa9bd819914db589f93d5f1e1ff4944ad4da
JasonGuoCi <geqianci@163.com>
2018-08-17 14:11:51 +0800

    thead test

---
+++ ClassLibrary1/ThreadExc/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadExc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadStart ts = new ThreadStart(ThreadEntity);
+            Thread worker = new Thread(ts);
+            worker.Start();
+            Console.WriteLine("Main Thread end"); ;
+            Console.ReadKey();
+        }
+
+        static void ThreadEntity()
+        {
+
+            Console.WriteLine("thread end");
+        }
+    }
+}
