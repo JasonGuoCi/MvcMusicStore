b6a0258f2ade513cb612c3022c511e1d37d62838
JasonGuoCi <geqianci@163.com>
2018-08-29 17:07:49 +0800

    Token绑定到ConfigureServices
    Token服务注册

--- ClassLibrary1/NetCoreTest/Startup.cs
+++ ClassLibrary1/NetCoreTest/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Swagger;
 using Microsoft.Extensions.PlatformAbstractions;
 using System.IO;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace NetCoreTest
 {
@@ -62,8 +63,41 @@ namespace NetCoreTest
                 var xmlModelPath = Path.Combine(basePath, "NetCoreTest.Model.xml");//NetCore.Model引用
                 c.IncludeXmlComments(xmlPath, true);//默认的第二个参数是false，这个是controller的注释，记得修改
                 c.IncludeXmlComments(xmlModelPath);
+
+                #region Token绑定到ConfigureServices
+
+                //添加header验证信息
+                var security = new Dictionary<string, IEnumerable<string>> { { "NetCoreTest", new string[] { } }, };
+                c.AddSecurityRequirement(security);
+
+                // //方案名称“NetCoreTest”可自定义，上下一致即可
+                c.AddSecurityDefinition("NetCoreTest", new ApiKeyScheme
+                {
+                    Description = "JWT授权(数据将在请求头中进行传输) 直接在下框中输入{token}\"",
+                    Name = "Authorization",//jwt默认的参数名称
+                    In = "header",//jwt默认存放Authorization信息的位置(请求头中)
+                    Type = "apiKey"
+                });
+
+                #endregion
             });
             #endregion
+
+            #region Token服务注册
+
+            services.AddSingleton<IMemoryCache>(factory =>
+            {
+                var cache = new MemoryCache(new MemoryCacheOptions());
+                return cache;
+            });
+
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Admin", policy => policy.RequireClaim("AdminType").Build());//注册权限管理，可以自定义多个
+            });
+            #endregion
+
         }
 
         /// <summary>
