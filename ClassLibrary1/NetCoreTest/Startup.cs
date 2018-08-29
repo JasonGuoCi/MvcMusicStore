cad8c2bc85b0d11e62d78f532bf1c5769db1e5e9
JasonGuoCi <geqianci@163.com>
2018-08-29 10:44:10 +0800

    添加 swagger 接口文档说明框架

--- ClassLibrary1/NetCoreTest/Startup.cs
+++ ClassLibrary1/NetCoreTest/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace NetCoreTest
 {
@@ -24,6 +25,22 @@ namespace NetCoreTest
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            //添加Swagger
+            #region Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v0.1.0",
+                    Title = "NetCoreTest",
+                    Description = "框架说明文档",
+                    TermsOfService = "None",
+                    Contact = new Swashbuckle.AspNetCore.Swagger.Contact { Name = "NetCoreTest", Email = "test@test.com", Url = "http://www.baidu.com" }
+
+                });
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +52,16 @@ namespace NetCoreTest
             }
 
             app.UseMvc();
+
+            //添加Swagger
+            #region Swagger
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiHelp V1");
+            });
+            #endregion
         }
     }
 }
